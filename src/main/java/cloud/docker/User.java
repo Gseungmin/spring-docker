658e430eb0a13282443813af23c24e2497e6d06d
Ji Seungmin <cswcsm02@gmail.com>
2023-03-16 03:19:46 +0900

    chore:H2에서 MySQL로 DB 변경

--- src/main/java/cloud/docker/User.java
+++ src/main/java/cloud/docker/User.java
@@ -1,14 +1,17 @@
 package cloud.docker;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor
 public class User {
 
     @Id @GeneratedValue
@@ -16,9 +19,6 @@ public class User {
     private Long id;
 
     private String name;
-
-    public User() {
-    }
 
     public User(String name) {
         this.name = name;
